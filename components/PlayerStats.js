4809d147826ab34b0359091c89e58847d60ff5c8
Miles Higbie <miles.higbie@gmail.com>
2020-05-10 18:51:42 -0400

    added detailed stats for players and stats section

--- components/PlayerStats.js
+++ components/PlayerStats.js
@@ -7,14 +7,22 @@ import { Opacity } from 'react-native-motion';
 export default function PlayerStats(props) {
     
     const [playerStats, setPlayerStats] = useState({})
-    const {birthCity, birthCountry, birthDate, birthStateProvince, height, primaryNumber, weight, shootsCatches} = playerStats
+    const [playerDetails, setPlayerDetails] = useState({})
+    const {birthCity, birthCountry, birthDate, birthStateProvince, height, primaryNumber, weight, shootsCatches} = playerDetails
+    const {games, goals, assists, points, timeOnIcePerGame} = playerStats
+    const pointsPerGame = (points / games).toFixed(2)
 
     useEffect(() => {
         fetch(`http://statsapi.web.nhl.com/${props.player.link}`)
         .then(response => response.json())
         .then(response => {
-            console.log(response.people[0])
-            setPlayerStats(response.people[0])
+            setPlayerDetails(response.people[0])
+        })
+        fetch(`https://statsapi.web.nhl.com/${props.player.link}/stats?stats=statsSingleSeason&season=20192020`)
+        .then(response => response.json())
+        .then(response => {
+            setPlayerStats(response.stats[0].splits[0].stat)
+            console.log(response.stats[0].splits[0].stat)
         })
       }, []);
 
@@ -26,8 +34,15 @@ export default function PlayerStats(props) {
        <Text style={styles.text}> Height:  {height}  </Text>
        <Text style={styles.text}> Weight:  {weight}  </Text>
        <Text style={styles.text}> Number:  {primaryNumber}  </Text>
-       {playerStats.primaryPosition && <Text style={styles.text}> Position: {playerStats.primaryPosition.name}  </Text>}
+       {playerDetails.primaryPosition && <Text style={styles.text}> Position: {playerDetails.primaryPosition.name}  </Text>}
        <Text style={styles.text}> Shoots:  {shootsCatches}  </Text>
+       <Text style={styles.statsHeader}> Stats </Text>
+       <Text style={styles.text}> Games:  {games}  </Text>
+       <Text style={styles.text}> Goals:  {goals}  </Text>
+       <Text style={styles.text}> Assists:  {assists}  </Text>
+       <Text style={styles.text}> Points:  {points}  </Text>
+       <Text style={styles.text}> Points Per Game:  {pointsPerGame}  </Text>
+       <Text style={styles.text}> Time On Ice Per Game:  {timeOnIcePerGame}  </Text>
     </View>
   );
 }
@@ -35,5 +50,13 @@ export default function PlayerStats(props) {
 const styles = StyleSheet.create({
     text: {
         color: "white"
+    }, 
+    statsHeader: {
+        color: "white",
+        fontSize: 30,
+        fontStyle: "italic",
+        marginTop: 10,
+        marginBottom: 10,
+        textAlign: "center"
     }
 });
