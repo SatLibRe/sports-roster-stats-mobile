0cd333b4639887e0168e7d25339c741388159546
Miles Higbie <miles.higbie@gmail.com>
2020-05-14 11:54:45 -0400

    added devils imformation for render

--- App.js
+++ App.js
@@ -2,25 +2,30 @@ import React, { useState } from 'react';
 import { SharedElement, TranslateYAndOpacity } from 'react-native-motion';
 import { StyleSheet, Text, View, Image, TouchableHighlight, ScrollView } from 'react-native';
 import Roster from "./containers/Roster.js"
+import ReactNativeComponentTree from 'react-native';
+
 
 export default function App() {
 
   const [rosterShown, setRosterShown] = useState(false)
+  const [currTeamId, setCurrTeamId] = useState("")
 
-  const displayRoster = () => {
+  const displayRosterSetId = (id) => {
     setRosterShown(!rosterShown)
+    setCurrTeamId(id)
   }
 
   return (
         <View style={styles.container}>
-            <TouchableHighlight onPress={displayRoster}>
+            <TouchableHighlight onPress={()=> displayRosterSetId(5)}>
               <Image 
+                  id= "5"
                   style={styles.pensLogo}
                   source={{
                   uri: 'https://www.logolynx.com/images/logolynx/ce/ce3fda47819cbae4635061d9dae31c6c.jpeg',
                 }}/>
             </TouchableHighlight>
-            <TouchableHighlight onPress={displayRoster}>
+            <TouchableHighlight onPress={()=> displayRosterSetId(1)}>
               <Image 
                   style={styles.pensLogo}
                   source={{
@@ -28,7 +33,7 @@ export default function App() {
                 }}/>
             </TouchableHighlight>
               {rosterShown ?  null : <Text style={styles.logoText}> Click the logo to display the current roster </Text>}
-              {rosterShown ?  <Roster/> : null}
+              {rosterShown ?  <Roster currTeamId={currTeamId}/> : null}
         </View>
   );
 }
--- containers/Roster.js
+++ containers/Roster.js
@@ -3,12 +3,14 @@
  import { SharedElement, TranslateYAndOpacity } from 'react-native-motion';
  import PlayerCard from "../components/PlayerCard.js"
 
- export default function Roster() {
+ export default function Roster(props) {
 
   const [roster, setRoster] = useState([])
 
+  console.log(props)
+
   useEffect(() => {
-    fetch("http://statsapi.web.nhl.com/api/v1/teams/5?expand=team.roster")
+    fetch(`http://statsapi.web.nhl.com/api/v1/teams/${props.currTeamId}?expand=team.roster`)
     .then(response => response.json())
     .then(response => {
       setRoster(response.teams[0].roster.roster)
@@ -17,7 +19,7 @@
 
    return (
      <ScrollView>
-       {roster.length > 0 && roster.map(player => <PlayerCard player={player.person}/>)}
+       {roster.length > 0 && roster.map(player => <PlayerCard key={player.person.id} player={player.person}/>)}
      </ScrollView>
    );
  }
